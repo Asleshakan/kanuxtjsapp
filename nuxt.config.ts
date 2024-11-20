36b36432b014be5b3909a0f876420ce00c3e7147
Asleshakan <142888325+Asleshakan@users.noreply.github.com>
2024-11-20 17:38:22 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  server: {
-    port: process.env.PORT || 8080, // Ensure it uses port 8080 or any available port set in environment
-    host: '0.0.0.0', // Bind to all available network interfaces
-  },
+  
 })
